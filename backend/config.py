12305e2410cb56568289f45215daed653f9a589a
anhleh33 <166407355+anhleh33@users.noreply.github.com>
2025-06-04 21:49:09 +0700

    change config link from me to quang

--- backend/config.py
+++ backend/config.py
@@ -9,13 +9,13 @@ SQL_SERVER = {
 }
 
 #H.Anh
-SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://@ANHLEHOANG333\\ANHHOANG/Food_System?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&encrypt=no&TrustServerCertificate=yes'
+# SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://@ANHLEHOANG333\\ANHHOANG/Food_System?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&encrypt=no&TrustServerCertificate=yes'
 
 # Quang
-# SQLALCHEMY_DATABASE_URI = (
-#     f"mssql+pyodbc://@{SQL_SERVER['server']}/{SQL_SERVER['database']}?"
-#     f"driver={SQL_SERVER['driver'].replace(' ', '+')}&trusted_connection=yes"
-# )
+SQLALCHEMY_DATABASE_URI = (
+    f"mssql+pyodbc://@{SQL_SERVER['server']}/{SQL_SERVER['database']}?"
+    f"driver={SQL_SERVER['driver'].replace(' ', '+')}&trusted_connection=yes"
+)
 
 #Gia Bao
 # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://DESKTOP-7VU15DP\SQLEXPRESS/Food_System?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes&encrypt=no&TrustServerCertificate=yes'
