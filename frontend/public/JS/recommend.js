6b413e1e91896313dff763495189962d714a27eb
QC2k4 <quang987xyz@gmail.com>
2025-06-09 20:52:32 +0700

    Thêm trending khi chưa đăng nhập

--- frontend/public/JS/recommend.js
+++ frontend/public/JS/recommend.js
@@ -1,12 +1,18 @@
 const renderRecommendations = async () => {
     const container = document.getElementById("recommendContainer");
+    const Claim = localStorage.getItem("Claim");
     const bmi = localStorage.getItem("userBMI");
     container.innerHTML = ""; // Clear previous content if needed
   
     try {
       var response = null;
-      if (bmi != "null"){
+      if (Claim){
+        if (bmi != "null") {
         response = await fetch(`http://localhost:5000/food/get-bmi-recommend?bmi=${bmi}`);
+        }
+        else {
+          response = await fetch("http://localhost:5000/food/get-random-recommend");
+        }
       }
       else {
         response = await fetch("http://localhost:5000/food/get-random-recommend");
